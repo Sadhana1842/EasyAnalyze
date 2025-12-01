f86075e26f515c350505cd4e141eaf20bca00d07
Sadhana_Srihari <93247093+Sadhana1842@users.noreply.github.com>
2025-12-01 19:37:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -334,7 +334,7 @@ if uploaded_file:
         y_axis = st.selectbox("Y-axis", options=numeric_cols)
         
         
-        if st.button("Generate Chart", use_container_width=True):
+        if st.button("Generate Chart", width="content"):
             fig, ax = plt.subplots(figsize=(10, 6))
             
             if chart_type == "Bar":
@@ -350,6 +350,7 @@ if uploaded_file:
 
 else:
     st.info("Upload an Excel file to get started.")
+
 
 
 
